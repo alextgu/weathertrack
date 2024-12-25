034e5cd49a033a7d1804fd4f6f7d9dd1005ec08c
Alexander Gu <agu@Alexanders-MacBook-Air.local>
2024-12-25 02:32:07 -0500

    updates

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ time_url = "https://worldtimeapi.org/api/timezone/America/Toronto"
 
 # API parameters
 lat, lon = 43.551915, -79.579578
-weather_api_key = "Weather_API_Key"
+weather_api_key = "801470731a163b794712c34ea5fd3a56" 
 
 # Fetch data from APIs
 weather_response = requests.get(
@@ -23,6 +23,10 @@ sun_info = sun_response.json()
 
 # Extract current time
 cur_time = time_info["datetime"][11:16]
+cur_hour = int(cur_time[:2])
+cur_minute = int(cur_time[3:5])
+
+cur_total = cur_hour*60 + cur_minute
 
 # Extract weather details
 temp = str(round(int(weather_info["main"]["temp"])))
@@ -31,8 +35,21 @@ weather_description = weather_info["weather"][0]["description"]
 
 # Extract and adjust sunrise and sunset times
 twelve = str(int(sun_info["results"]["sunset"][0]) + 12)
-sunset = twelve + sun_info["results"]["sunset"][1:7]
-sunrise = sun_info["results"]["sunrise"][:7]
+sunset_time = twelve + sun_info["results"]["sunset"][1:7]
+sunrise_time = sun_info["results"]["sunrise"][:7]
+
+if len(sunset_time) == 7:
+    sunset_time = "0" + sunset_time
+if len(sunrise_time) == 7:
+    sunrise_time = "0" + sunrise_time
+
+set_hour = int(sunset_time[:2])
+set_minute = int(sunset_time[3:5])
+rise_hour = int(sunrise_time[:2])
+rise_minute = int(sunrise_time[3:5])
+
+set_total = set_hour*60 + set_minute
+rise_total = rise_hour*60 + rise_minute
 
 # Determine weather intensity
 if "light" in weather_description:
@@ -42,8 +59,16 @@ elif "heavy" in weather_description or "extreme" in weather_description:
 else:
     intensity = 1
 
-full_description = "Home: "
+# Determine sun position
+if cur_total < rise_total:
+    sun_degree = -1  
+elif cur_total > set_total:
+    sun_degree = -1  
+else:
+    sun_degree = 180 - (((cur_total - rise_total) / (set_total - rise_total)) * 180)
+
 # Generate full description
+full_description = cur_time + ". Home: "
 if weather_type == "Clouds":
     full_description += f"It is {temp} degrees Celsius. There are {weather_description}."
 elif weather_type == "Snow":
@@ -55,5 +80,7 @@ else:
 print(full_description)
 
 """
-
+weather intensity - 0,1,2
+weather type - rain, clouds, sun, snow etc
+sun angle set - 0-180 or -1 if sun has not risen or has fallen
 """
